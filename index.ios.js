05d83f8d1156078fa6843bd8cddffb95924ddb76
胡超 <statham@huchaodeMac-mini.local>
2017-07-27 14:59:51 +0800

    IOS

--- index.ios.js
+++ index.ios.js
@@ -8,7 +8,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
 } from 'react-native';
-import App from './index';
+import App from './App';
 
 export default class RNHelloWord extends Component {
   render() {
