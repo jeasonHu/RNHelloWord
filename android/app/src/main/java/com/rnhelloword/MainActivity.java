fe66243ad732767d34244f1a64d1e0bdf69b8a85
胡超 <statham@huchaodeMac-mini.local>
2018-01-16 21:43:48 +0800

    no message

--- android/app/src/main/java/com/rnhelloword/MainActivity.java
+++ android/app/src/main/java/com/rnhelloword/MainActivity.java
@@ -2,7 +2,7 @@ package com.rnhelloword;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class RNMainActivity extends ReactActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
